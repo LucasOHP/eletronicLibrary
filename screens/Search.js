b0c7bf9a866bc88d296262e8c80322d5d6e21932
LucasOHP <92893161+LucasOHP@users.noreply.github.com>
2022-07-20 16:47:14 -0300

    aula 75

---
+++ screens/Search.js
@@ -0,0 +1,31 @@
+import react from "react";
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+} from "react-native";
+
+
+export default class SearchScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>Search screen</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#5653d4"
+    },
+    text:{
+        backgroundColor: 'lightgray',
+        fontSize: 30,
+    }
+})
---
+++ screens/Transaction.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+} from "react-native";
+
+
+export default class TransactionScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>Transaction screen</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#5653d4"
+    },
+    text:{
+        backgroundColor: 'lightgray',
+        fontSize: 30,
+    }
+})
